6e38657a3057b96604efb8327ca2104d050a5239
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-11 18:15:47 -0700

    finihsed scheme model and mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,52 @@
-const db =require("../data/")
+const db = require("../data/dbConfig");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+    remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .orderBy("steps.step_number")
+    .where("schemes.id", id);
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id){
+    return db("schemes")
+    .where('id', id)
+    .del()
+}
